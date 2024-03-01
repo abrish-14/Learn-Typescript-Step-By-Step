4cd38f2c7f3c1b1531bc4bd35583d5c4ce8ea2a7
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-01 23:22:30 +0500

    Add files via upload

---
+++ Datatype Assignement/Datatype.ts
@@ -0,0 +1,31 @@
+// Boolean
+let isDone: boolean = false;
+
+console.log(isDone);
+
+// Number
+let decimal: number = 6;
+let binary: number = 0b1010;
+
+console.log(decimal+binary);
+// String
+let color: string = "black";
+let fullName: string = `Abrish Rathoor `;
+let sentence: string = `Hello, my name is ${fullName}.`;
+
+console.log(color+fullName);
+
+//null and Undefined
+let u: undefined = undefined;
+let n: null = null;
+
+console.log(n);
+
+// Symbol
+let secretSymbol = Symbol("treasure");
+let Key = Symbol("key");
+
+console.log(`key`);
+
+
+
