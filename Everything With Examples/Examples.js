cea81e3357153771b90ae05578148e936543a71b
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-08 19:48:00 +0500

    Add files via upload

---
+++ Everything With Examples/Examples.js
@@ -0,0 +1,21 @@
+//HERE IS EVRYTHING WITH EXAMPLE I HAVE LEARN IN CLASS 
+// 1. EXAMPLE OF VARIABLE NAMING CONVENTIONS:
+// "camelCase":
+var personName = "Abrish";
+console.log(personName);
+// "snake_case":
+var person_name = "Madiha";
+console.log(person_name);
+// "PascalCase":
+var PersonName = "Rafea";
+console.log(PersonName);
+// 3. EXAMPLE OF ARRAYS:
+var bestFriend = ["Madiha", "Maliha", "Laiba"];
+console.log(bestFriend);
+console.log("MY bestie is" + bestFriend[0]);
+var favritFruit = ["Watermelon", "Mango", "bananana", "Apple"];
+console.log(favritFruit);
+console.log(favritFruit[0], favritFruit[4], favritFruit[2], +"are my favourite fruits");
+var myAge = [4, 5, 17, 9];
+console.log(myAge);
+console.log("I am" + myAge[2] + "years old");
