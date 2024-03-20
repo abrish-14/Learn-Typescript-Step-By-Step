16de3d02a204e4efe99d8e3f84dc2c124c7d3163
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-20 20:02:40 +0500

    Add files via upload

---
+++ 02 VARIABLE ASSIGNEMENT/Variable.ts
@@ -0,0 +1,13 @@
+// 1. EXAMPLE OF VARIABLE NAMING CONVENTIONS:
+
+// "camelCase":
+let girlName : string = "Abrish";
+console.log(girlName);
+
+// "snake_case":
+let girl_name : string= "Madiha";
+console.log(girl_name);
+
+// "PascalCase":
+let GirlName : string = "Rafea";
+console.log(GirlName);
