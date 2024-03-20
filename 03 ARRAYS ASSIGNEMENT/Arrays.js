75adb70a84b5eb28d0ea07a0ff866ea1ae2a42a9
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-20 20:04:32 +0500

    Add files via upload

---
+++ 03 ARRAYS ASSIGNEMENT/Arrays.js
@@ -0,0 +1,10 @@
+// EXAMPLE OF ARRAYS:
+var bestFriend = ["Madiha", "Maliha", "Laiba"];
+console.log(bestFriend);
+console.log("MY bestie is" + bestFriend[0]);
+var favritFruit = ["Watermelon", "Mango", "bananana", "Apple"];
+console.log(favritFruit);
+console.log(favritFruit[0], favritFruit[4], favritFruit[2], +"are my favourite fruits");
+var myAge = [4, 5, 17, 9];
+console.log(myAge);
+console.log("I am" + myAge[2] + "years old");
---
+++ 03 ARRAYS ASSIGNEMENT/Arrays.ts
@@ -0,0 +1,13 @@
+// EXAMPLE OF ARRAYS:
+
+let bestFriend : string[]  = ["Madiha", "Maliha", "Laiba"];
+console.log(bestFriend);
+console.log("MY bestie is"+ bestFriend[0]);
+
+let favritFruit : string[]  = ["Watermelon", "Mango", "bananana" ,"Apple"];
+console.log(favritFruit);
+console.log(favritFruit[0] , favritFruit[4] ,favritFruit[2] , + "are my favourite fruits");
+
+let myAge : number[] = [4, 5, 17, 9];
+console.log(myAge);
+console.log("I am" + myAge[2] +  "years old" );
